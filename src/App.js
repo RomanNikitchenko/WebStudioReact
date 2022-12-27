c2da79507188be9125d2e90ce43f6b61fbc3a876
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-12-27 15:08:57 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { AppBar } from 'components/AppBar';
 import { ProductDetails } from './views/ProductDetails';
 import HomeViev from './views/HomeView';
 import PortfolioView from './views/PortfolioView';
+import { getCurrentType } from 'redux/extraInfo/extraInfo-selectors';
+import { useSelector } from 'react-redux';
 
 const App = () => {
-
+  const transactionOptions = useSelector(getCurrentType);
   return (
     <div>
       <Routes>
         <Route path="/" element={<AppBar />}>
           <Route index element={<HomeViev />} />
           <Route path="portfolio" element={<PortfolioView />}>
-            <Route index element={<Navigate to="All" />} />
-            <Route path=":id" element={<ProductDetails/>} />
+            <Route index element={<Navigate to={`${transactionOptions}`} />} />
+            <Route path=":id" element={<ProductDetails />} />
           </Route>
           <Route path="contacts" element={<div>contacts</div>} />
           <Route path="*" element={<div>*</div>} />

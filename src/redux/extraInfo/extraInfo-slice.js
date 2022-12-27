69499679ff4cf45b31a141c93caf99de037dc64a
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-12-27 16:24:22 +0200

    PortfolioProduct styled

--- src/redux/extraInfo/extraInfo-slice.js
+++ src/redux/extraInfo/extraInfo-slice.js
@@ -1,58 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// const year = String(new Date().getFullYear());
-// const month = String(new Date().getMonth() + 1);
-// const day = String(new Date().getDate());
-
 const initialState = {
-  // date: { day, month, year },
   currentType: 'All',
-  // currentCategory: "Категорія",
 };
 
 const dateSlice = createSlice({
   name: 'extraInfo',
   initialState,
   reducers: {
-    // addDate: (state, action) => {
-    //   state.date = action.payload;
-    // },
-
     addCurrentType: (state, action) => {
       state.currentType = action.payload;
     },
-
-    // addCurrentCategory: (state, action) => {
-    //   state.currentCategory = action.payload;
-    // },
-
-    // goBackOneMonth: (state, action) => {
-    //   if (Number(state.date.month) === 1) {
-    //     state.date.year = Number(state.date.year) - 1;
-    //     state.date.month = 12;
-    //     return;
-    //   }
-
-    //   state.date.month = Number(state.date.month) - 1;
-    // },
-    // goForwardOneMonth: (state, action) => {
-    //   if (Number(state.date.month) === 12) {
-    //     state.date.year = Number(state.date.year) + 1;
-    //     state.date.month = 1;
-    //     return;
-    //   }
-
-    //   state.date.month = Number(state.date.month) + 1;
-    // },
   },
 });
 
-export const {
-  // addDate,
-  addCurrentType,
-  // addCurrentCategory,
-  // goBackOneMonth,
-  // goForwardOneMonth,
-} = dateSlice.actions;
+export const { addCurrentType } = dateSlice.actions;
 
 export default dateSlice.reducer;
--- src/views/ProductDetails.js
+++ src/views/ProductDetails.js
@@ -1,6 +1,29 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductById } from '../fakeAPI';
+import styled from 'styled-components';
+
+const WorksList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -30px;
+  margin-top: -30px;
+`;
+
+const PortfolioProduct = styled.li`
+  @media screen and (min-width: 480px) {
+    flex-basis: calc(100% / 1 - 30px);
+  }
+  @media screen and (min-width: 768px) {
+    flex-basis: calc(100% / 2 - 30px);
+  }
+  @media screen and (min-width: 1200px) {
+    flex-basis: calc(100% / 3 - 30px);
+  }
+
+  margin-left: 30px;
+  margin-top: 30px;
+`;
 
 export const ProductDetails = () => {
   const { id } = useParams();
@@ -11,23 +34,23 @@ export const ProductDetails = () => {
   }, [setProducts, id]);
 
   return (
-    <ul>
+    <WorksList>
       {product &&
         product.map(({ id, img, description, title, text }) => {
           // const { srcset, sizes, src, alt } = img;
           return (
-            <li key={id}>
+            <PortfolioProduct key={id}>
               <div>
-                {/* <img srcset={srcset} sizes={sizes} src={src} alt={alt}/> */}
+                {/* <img srcset={srcset} sizes={sizes} src={src} alt={alt} /> */}
                 <p>{description}</p>
               </div>
               <div>
                 <h2>{title}</h2>
                 <p>{text}</p>
               </div>
-            </li>
+            </PortfolioProduct>
           );
         })}
-    </ul>
+    </WorksList>
   );
 };

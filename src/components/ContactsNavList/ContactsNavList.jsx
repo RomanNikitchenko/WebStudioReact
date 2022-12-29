d1b0b952b6c905c4e728329818826c69e8e5da6a
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-12-29 16:31:08 +0200

    styled

--- src/components/ContactsNavList/ContactsNavList.jsx
+++ src/components/ContactsNavList/ContactsNavList.jsx
@@ -1,46 +1,11 @@
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-
-const List = styled.ul`
-  padding: 0;
-  margin: 0;
-  margin-left: auto;
-  list-style: none;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
-const Item = styled.li`
-  &:not(:last-child) {
-    margin-right: 30px;
-  }
-`;
-
-const StyledLink = styled(NavLink)`
-  text-decoration: none;
-  color: var(--title-text-color);
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 1.14;
-  letter-spacing: 0.02em;
-
-  &.active {
-    color: var(--accent-color);
-  }
-
-  &:hover {
-    color: var(--accent-color);
-  }
-`;
+import s from './ContactsNavList.module.css';
 
 export const ContactsNavList = () => {
   return (
-    <List>
-      <Item>
-        <StyledLink to="/mailto:info@devstudio.com">
+    <ul className={s.list}>
+      <li className={s.item}>
+        <NavLink className={s.link} to="/mailto:info@devstudio.com">
           {/* <svg class="contact-nav__icon" width="14" height="10">
             <use href="./images/icons.svg#envelope2"></use>
           </svg>
@@ -48,11 +13,11 @@ export const ContactsNavList = () => {
             <use href="./images/icons.svg#envelope"></use>
           </svg> */}
           info@devstudio.com
-        </StyledLink>
-      </Item>
+        </NavLink>
+      </li>
 
-      <Item>
-        <StyledLink to="/tel:+380961111111">
+      <li className={s.item}>
+        <NavLink className={s.link} to="/tel:+380961111111">
           {/* <svg class="contact-nav__icon" width="10" height="14">
             <use href="./images/icons.svg#smartphone"></use>
           </svg>
@@ -60,8 +25,8 @@ export const ContactsNavList = () => {
             <use href="./images/icons.svg#smartphone2"></use>
           </svg> */}
           +38 096 111 11 11
-        </StyledLink>
-      </Item>
-    </List>
+        </NavLink>
+      </li>
+    </ul>
   );
 };

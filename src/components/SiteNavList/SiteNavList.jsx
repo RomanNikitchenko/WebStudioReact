1d3053f625f16662aae537f88879cb000919ec9a
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-12-29 14:45:54 +0200

    styles

--- src/components/SiteNavList/SiteNavList.jsx
+++ src/components/SiteNavList/SiteNavList.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import s from './SiteNavList.module.css';
 
 const List = styled.ul`
   padding: 0;
@@ -16,56 +17,38 @@ const Item = styled.li`
   }
 `;
 
-const StyledLink = styled(NavLink)`
-  text-decoration: none;
-  color: var(--title-text-color);
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 1.14;
-  letter-spacing: 0.02em;
-  position: relative;
-
-  &::after {
-    content: ' ';
-    position: absolute;
-    left: 50%;
-    bottom: -31px;
-    width: 0px;
-    min-height: 4px;
-    border-radius: 4px;
-    background-color: var(--accent-color);
-    transform: translate(-50%, -0%);
-    transition: width 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-
-  &.active {
-    color: var(--accent-color);
-
-    &::after {
-      content: ' ';
-      width: 100%;
-    }
-  }
-
-  :hover:not(.active),
-  :focus-visible:not(.active) {
-    color: var(--accent-color);
-  }
-`;
-
 export const SiteNavList = () => {
   return (
     <List>
       <Item>
-        <StyledLink to="/">Студия</StyledLink>
+        <NavLink
+          to="/"
+          className={isActive => {
+            return isActive.isActive ? `${s.link} ${s.active}` : `${s.link}`;
+          }}
+        >
+          Студия
+        </NavLink>
       </Item>
       <Item>
-        <StyledLink to="/portfolio">Портфолио</StyledLink>
+        <NavLink
+          to="/portfolio"
+          className={isActive => {
+            return isActive.isActive ? `${s.link} ${s.active}` : `${s.link}`;
+          }}
+        >
+          Портфолио
+        </NavLink>
       </Item>
       <Item>
-        <StyledLink to="/contacts">Контакты</StyledLink>
+        <NavLink
+          to="/contacts"
+          className={isActive => {
+            return isActive.isActive ? `${s.link} ${s.active}` : `${s.link}`;
+          }}
+        >
+          Контакты
+        </NavLink>
       </Item>
     </List>
   );

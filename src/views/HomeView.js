46a239716b5a950808dad7e968d6ae1e1817b06a
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-12-30 16:26:08 +0200

    s

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const styles = {
   container: {
-    minHeight: 'calc(100vh - 50px)',
+    minHeight: 'calc(100vh - 200px)',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',

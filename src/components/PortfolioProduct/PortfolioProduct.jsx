e30407677f80e8f51302c5afbcd865450394d546
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-01-30 23:31:32 +0200

    img

--- src/components/PortfolioProduct/PortfolioProduct.jsx
+++ src/components/PortfolioProduct/PortfolioProduct.jsx
@@ -32,19 +32,28 @@ export const PortfolioProduct = ({
       <NavLink onClick={e => e.preventDefault()} className={s.portfolioLink}>
         <div className={s.portfolioDescription}>
           <picture>
-            <source
+            {/* {370px} */}
+            {/* <source
               srcSet={`${img.desktop1x} 1x, ${img.desktop2x} 2x`}
               media="(min-width: 1200px)"
-            />
-            <source
+            /> */}
+            {/* {354px} */}
+            {/* <source
               srcSet={`${img.tablet1x} 1x, ${img.tablet2x} 2x`}
               media="(min-width: 768px)"
-            />
-            <source
+            /> */}
+            {/* {450px} */}
+            {/* <source
               srcSet={`${img.mobile1x} 1x, ${img.mobile2x} 2x`}
               media="(max-width: 767px)"
+            /> */}
+
+            <img
+              srcSet={`${img.desktop1x} 370w, ${img.desktop2x} 740w, ${img.tablet1x} 354w, ${img.tablet2x} 708w, ${img.mobile1x} 450w, ${img.mobile2x} 900w,`}
+              sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (max-width: 767px) 450px, 100vw"
+              src={img.src}
+              alt={img.alt}
             />
-            <img className={s.portfolioImg} src={img.src} alt={img.alt} />
           </picture>
           <p className={s.portfolioText}>{description}</p>
         </div>
--- src/fakeAPI.js
+++ src/fakeAPI.js
@@ -1,38 +1,65 @@
 import img1ListItem354 from 'assets/images/img1-list-item354.jpg';
+import img1ListItem354F2x from 'assets/images/img1-list-item354@2x.jpg';
 import img1ListItem370 from 'assets/images/img1-list-item370.jpg';
+import img1ListItem370F2x from 'assets/images/img1-list-item370@2x.jpg';
 import img1ListItem450 from 'assets/images/img1-list-item450.jpg';
+import img1ListItem450F2x from 'assets/images/img1-list-item450@2x.jpg';
 
 import img2ListItem354 from 'assets/images/img2-list-item354.jpg';
+import img2ListItem354F2x from 'assets/images/img2-list-item354@2x.jpg';
 import img2ListItem370 from 'assets/images/img2-list-item370.jpg';
+import img2ListItem370F2x from 'assets/images/img2-list-item370@2x.jpg';
 import img2ListItem450 from 'assets/images/img2-list-item450.jpg';
+import img2ListItem450F2x from 'assets/images/img2-list-item450@2x.jpg';
 
 import img3ListItem354 from 'assets/images/img3-list-item354.jpg';
+import img3ListItem354F2x from 'assets/images/img3-list-item354@2x.jpg';
 import img3ListItem370 from 'assets/images/img3-list-item370.jpg';
+import img3ListItem370F2x from 'assets/images/img3-list-item370@2x.jpg';
 import img3ListItem450 from 'assets/images/img3-list-item450.jpg';
+import img3ListItem450F2x from 'assets/images/img3-list-item450@2x.jpg';
 
 import img4ListItem354 from 'assets/images/img4-list-item354.jpg';
+import img4ListItem354F2x from 'assets/images/img4-list-item354@2x.jpg';
 import img4ListItem370 from 'assets/images/img4-list-item370.jpg';
+import img4ListItem370F2x from 'assets/images/img4-list-item370@2x.jpg';
 import img4ListItem450 from 'assets/images/img4-list-item450.jpg';
+import img4ListItem450F2x from 'assets/images/img4-list-item450@2x.jpg';
 
 import img5ListItem354 from 'assets/images/img5-list-item354.jpg';
+import img5ListItem354F2x from 'assets/images/img5-list-item354@2x.jpg';
 import img5ListItem370 from 'assets/images/img5-list-item370.jpg';
+import img5ListItem370F2x from 'assets/images/img5-list-item370@2x.jpg';
 import img5ListItem450 from 'assets/images/img5-list-item450.jpg';
+import img5ListItem450F2x from 'assets/images/img5-list-item450@2x.jpg';
 
 import img6ListItem354 from 'assets/images/img6-list-item354.jpg';
+import img6ListItem354F2x from 'assets/images/img6-list-item354@2x.jpg';
 import img6ListItem370 from 'assets/images/img6-list-item370.jpg';
+import img6ListItem370F2x from 'assets/images/img6-list-item370@2x.jpg';
 import img6ListItem450 from 'assets/images/img6-list-item450.jpg';
+import img6ListItem450F2x from 'assets/images/img6-list-item450@2x.jpg';
 
 import img7ListItem354 from 'assets/images/img7-list-item354.jpg';
+import img7ListItem354F2x from 'assets/images/img7-list-item354@2x.jpg';
 import img7ListItem370 from 'assets/images/img7-list-item370.jpg';
+import img7ListItem370F2x from 'assets/images/img7-list-item370@2x.jpg';
 import img7ListItem450 from 'assets/images/img7-list-item450.jpg';
+import img7ListItem450F2x from 'assets/images/img7-list-item450@2x.jpg';
 
 import img8ListItem354 from 'assets/images/img8-list-item354.jpg';
+import img8ListItem354F2x from 'assets/images/img8-list-item354@2x.jpg';
 import img8ListItem370 from 'assets/images/img8-list-item370.jpg';
+import img8ListItem370F2x from 'assets/images/img8-list-item370@2x.jpg';
 import img8ListItem450 from 'assets/images/img8-list-item450.jpg';
+import img8ListItem450F2x from 'assets/images/img8-list-item450@2x.jpg';
 
 import img9ListItem354 from 'assets/images/img9-list-item354.jpg';
+import img9ListItem354F2x from 'assets/images/img9-list-item354@2x.jpg';
 import img9ListItem370 from 'assets/images/img9-list-item370.jpg';
+import img9ListItem370F2x from 'assets/images/img9-list-item370@2x.jpg';
 import img9ListItem450 from 'assets/images/img9-list-item450.jpg';
+import img9ListItem450F2x from 'assets/images/img9-list-item450@2x.jpg';
 
 const filterBtn = [
   { id: 'All', name: 'Все' },
@@ -48,12 +75,12 @@ const products = [
     name: ['All', 'Web-sites'],
     img: {
       mobile1x: img1ListItem450,
-      mobile2x: img1ListItem450,
+      mobile2x: img1ListItem450F2x,
       tablet1x: img1ListItem354,
-      tablet2x: img1ListItem354,
+      tablet2x: img1ListItem354F2x,
       desktop1x: img1ListItem370,
-      desktop2x: img1ListItem370,
-      src: img1ListItem354,
+      desktop2x: img1ListItem370F2x,
+      src: img1ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -66,12 +93,12 @@ const products = [
     name: ['All', 'Design'],
     img: {
       mobile1x: img2ListItem450,
-      mobile2x: img2ListItem450,
+      mobile2x: img2ListItem450F2x,
       tablet1x: img2ListItem354,
-      tablet2x: img2ListItem354,
+      tablet2x: img2ListItem354F2x,
       desktop1x: img2ListItem370,
-      desktop2x: img2ListItem370,
-      src: img2ListItem354,
+      desktop2x: img2ListItem370F2x,
+      src: img2ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -84,12 +111,12 @@ const products = [
     name: ['All', 'Applications'],
     img: {
       mobile1x: img3ListItem450,
-      mobile2x: img3ListItem450,
+      mobile2x: img3ListItem450F2x,
       tablet1x: img3ListItem354,
-      tablet2x: img3ListItem354,
+      tablet2x: img3ListItem354F2x,
       desktop1x: img3ListItem370,
-      desktop2x: img3ListItem370,
-      src: img3ListItem354,
+      desktop2x: img3ListItem370F2x,
+      src: img3ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -102,12 +129,12 @@ const products = [
     name: ['All', 'Marketing'],
     img: {
       mobile1x: img4ListItem450,
-      mobile2x: img4ListItem450,
+      mobile2x: img4ListItem450F2x,
       tablet1x: img4ListItem354,
-      tablet2x: img4ListItem354,
+      tablet2x: img4ListItem354F2x,
       desktop1x: img4ListItem370,
-      desktop2x: img4ListItem370,
-      src: img4ListItem354,
+      desktop2x: img4ListItem370F2x,
+      src: img4ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -120,12 +147,12 @@ const products = [
     name: ['All', 'Applications'],
     img: {
       mobile1x: img5ListItem450,
-      mobile2x: img5ListItem450,
+      mobile2x: img5ListItem450F2x,
       tablet1x: img5ListItem354,
-      tablet2x: img5ListItem354,
+      tablet2x: img5ListItem354F2x,
       desktop1x: img5ListItem370,
-      desktop2x: img5ListItem370,
-      src: img5ListItem354,
+      desktop2x: img5ListItem370F2x,
+      src: img5ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -138,12 +165,12 @@ const products = [
     name: ['All', 'Web-sites'],
     img: {
       mobile1x: img6ListItem450,
-      mobile2x: img6ListItem450,
+      mobile2x: img6ListItem450F2x,
       tablet1x: img6ListItem354,
-      tablet2x: img6ListItem354,
+      tablet2x: img6ListItem354F2x,
       desktop1x: img6ListItem370,
-      desktop2x: img6ListItem370,
-      src: img6ListItem354,
+      desktop2x: img6ListItem370F2x,
+      src: img6ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -156,12 +183,12 @@ const products = [
     name: ['All', 'Design'],
     img: {
       mobile1x: img7ListItem450,
-      mobile2x: img7ListItem450,
+      mobile2x: img7ListItem450F2x,
       tablet1x: img7ListItem354,
-      tablet2x: img7ListItem354,
+      tablet2x: img7ListItem354F2x,
       desktop1x: img7ListItem370,
-      desktop2x: img7ListItem370,
-      src: img7ListItem354,
+      desktop2x: img7ListItem370F2x,
+      src: img7ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -174,12 +201,12 @@ const products = [
     name: ['All', 'Marketing'],
     img: {
       mobile1x: img8ListItem450,
-      mobile2x: img8ListItem450,
+      mobile2x: img8ListItem450F2x,
       tablet1x: img8ListItem354,
-      tablet2x: img8ListItem354,
+      tablet2x: img8ListItem354F2x,
       desktop1x: img8ListItem370,
-      desktop2x: img8ListItem370,
-      src: img8ListItem354,
+      desktop2x: img8ListItem370F2x,
+      src: img8ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:
@@ -192,12 +219,12 @@ const products = [
     name: ['All', 'Applications'],
     img: {
       mobile1x: img9ListItem450,
-      mobile2x: img9ListItem450,
+      mobile2x: img9ListItem450F2x,
       tablet1x: img9ListItem354,
-      tablet2x: img9ListItem354,
+      tablet2x: img9ListItem354F2x,
       desktop1x: img9ListItem370,
-      desktop2x: img9ListItem370,
-      src: img9ListItem354,
+      desktop2x: img9ListItem370F2x,
+      src: img9ListItem450,
       alt: 'Технокряк Веб-сайт',
     },
     description:

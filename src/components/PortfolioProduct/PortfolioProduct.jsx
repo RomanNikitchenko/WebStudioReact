23b9d6bbc94eece43355580c81a95e4d0ba6ef81
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-01-30 12:22:32 +0200

    Update PortfolioProduct.jsx

--- src/components/PortfolioProduct/PortfolioProduct.jsx
+++ src/components/PortfolioProduct/PortfolioProduct.jsx
@@ -13,41 +13,37 @@ export const PortfolioProduct = ({
   index,
   options,
 }) => {
-  const isDesktopAndTablet = useMediaQuery(
+  const isTablet = useMediaQuery(
     'only screen and (min-width: 768px) and (max-width: 1199px)'
   );
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (title.length > 28 && isDesktopAndTablet === true) {
+    if (title.length > 28 && isTablet === true) {
       dispatch(changeCurrentType(index % 2 === 1 ? index - 1 : index + 1));
-    } else if (title.length > 28 && isDesktopAndTablet === false) {
+    } else if (title.length > 28 && isTablet === false) {
       dispatch(changeCurrentType([]));
     }
-  }, [dispatch, index, title.length, isDesktopAndTablet]);
+  }, [dispatch, index, title.length, isTablet]);
 
   return (
     <li className={s.portfolioItem}>
       <NavLink onClick={e => e.preventDefault()} className={s.portfolioLink}>
         <div className={s.portfolioDescription}>
           <picture>
-            {/* 370px */}
             <source
               srcSet={`${img.desktop1x} 1x, ${img.desktop2x} 2x`}
               media="(min-width: 1200px)"
             />
-            {/* 354px */}
             <source
               srcSet={`${img.tablet1x} 1x, ${img.tablet2x} 2x`}
               media="(min-width: 768px)"
             />
-            {/* 450px */}
             <source
               srcSet={`${img.mobile1x} 1x, ${img.mobile2x} 2x`}
               media="(max-width: 767px)"
             />
-            {/* 354px */}
             <img className={s.portfolioImg} src={img.src} alt={img.alt} />
           </picture>
           <p className={s.portfolioText}>{description}</p>

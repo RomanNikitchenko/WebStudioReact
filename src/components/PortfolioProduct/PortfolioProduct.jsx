5d3ecd2cb06d2321704e2b310971d1b4b44035ce
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-01-31 15:56:52 +0200

    Update PortfolioProduct.jsx

--- src/components/PortfolioProduct/PortfolioProduct.jsx
+++ src/components/PortfolioProduct/PortfolioProduct.jsx
@@ -32,30 +32,26 @@ export const PortfolioProduct = ({
     <li className={s.portfolioItem}>
       <NavLink onClick={e => e.preventDefault()} className={s.portfolioLink}>
         <div className={s.portfolioDescription}>
-          {/* <picture> */}
-          {/* {370px} */}
-          {/* <source
-              srcSet={`${img.desktop1x} 1x, ${img.desktop2x} 2x`}
-              media="(min-width: 1200px)"
-            /> */}
-          {/* {354px} */}
-          {/* <source
-              srcSet={`${img.tablet1x} 1x, ${img.tablet2x} 2x`}
-              media="(min-width: 768px)"
-            /> */}
-          {/* {450px} */}
-          {/* <source
-              srcSet={`${img.mobile1x} 1x, ${img.mobile2x} 2x`}
-              media="(max-width: 767px)"
-            /> */}
-
-          {/* <img
-              srcSet={`${img.desktop1x} 370w, ${img.desktop2x} 740w, ${img.tablet1x} 354w, ${img.tablet2x} 708w, ${img.mobile1x} 450w, ${img.mobile2x} 900w,`}
-              sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (max-width: 767px) 450px, 100vw"
-              src={img.src}
-              alt={img.alt}
-            /> */}
-          {/* </picture> */}
+          {/* <picture>
+            <source
+                srcSet={`${img.desktop1x} 1x, ${img.desktop2x} 2x`}
+                media="(min-width: 1200px)"
+              />
+            <source
+                srcSet={`${img.tablet1x} 1x, ${img.tablet2x} 2x`}
+                media="(min-width: 768px)"
+              />
+            <source
+                srcSet={`${img.mobile1x} 1x, ${img.mobile2x} 2x`}
+                media="(max-width: 767px)"
+              />
+            <img
+                srcSet={`${img.desktop1x} 370w, ${img.desktop2x} 740w, ${img.tablet1x} 354w, ${img.tablet2x} 708w, ${img.mobile1x} 450w, ${img.mobile2x} 900w,`}
+                sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (max-width: 767px) 450px, 100vw"
+                src={img.src}
+                alt={img.alt}
+              />
+          </picture> */}
           <LazyImage
             srcSet={`${img.desktop1x} 370w, ${img.desktop2x} 740w, ${img.tablet1x} 354w, ${img.tablet2x} 708w, ${img.mobile1x} 450w, ${img.mobile2x} 900w,`}
             sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (max-width: 767px) 450px, 100vw"

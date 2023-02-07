2308abfa70c5c607ad92a1e4171c4a3e0203a78c
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-02-07 16:38:31 +0200

    img

--- src/lazy-image.js
+++ src/lazy-image.js
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import LazyLoad from 'vanilla-lazyload';
 import lazyloadConfig from './config/lazyioad';
 import { useMediaQuery } from '@react-hook/media-query';
+import './lazy-image.css';
 
 if (!document.lazyLoadInstance) {
   document.lazyLoadInstance = new LazyLoad(lazyloadConfig);
 }
 
-const LazyImage = ({ portfolioImg, img, isHidden }) => {
-  const [imgHidden, setImgHidden] = useState(true);
-
+const LazyImage = ({ portfolioImg, img }) => {
   const isMobile = useMediaQuery('only screen and (max-width: 767px)');
   const isTablet = useMediaQuery(
     'only screen and (min-width: 768px) and (max-width: 1199px)'
@@ -17,7 +16,6 @@ const LazyImage = ({ portfolioImg, img, isHidden }) => {
   const isDesktop = useMediaQuery('only screen and (min-width: 1200px)');
 
   const {
-    mobileBase450px,
     alt,
     mobile1x,
     tablet1x,
@@ -25,11 +23,13 @@ const LazyImage = ({ portfolioImg, img, isHidden }) => {
     mobile2x,
     tablet2x,
     desktop2x,
+    mobileBase450px,
+    tableBase354px,
+    desktopBase370px,
   } = img;
 
   useEffect(() => {
     document.lazyLoadInstance.update();
-    setImgHidden(false);
   }, []);
 
   const imageDevice = () => {
@@ -49,10 +49,10 @@ const LazyImage = ({ portfolioImg, img, isHidden }) => {
       return mobileBase450px;
     }
     if (isTablet) {
-      return 'data:image/gif;base64,R0lGODlhOwAxAIAAAP///wAAACH5BAEAAAEALAAAAAA7ADEAAAI4jI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyTNf2jef6zvf+DwwKh8Si8YhMKpfMpvPZKgAAOw==';
+      return tableBase354px;
     }
     if (isDesktop) {
-      return 'data:image/gif;base64,R0lGODlhuQCTAIAAAP///wAAACH5BAEAAAEALAAAAAC5AJMAAALPjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyTNf2jef6zvf+DwwKh8Si8YhMKpfMpvMJjUqn1Kr1is1qt9yu9wsOi8fksvmMTqvX7Lb7DY/L5/S6/Y7P6/f8vv8PGCg4SFhoeIiYqLjI2Oj4CBkpOUlZaXmJmam5ydnp+QkaKjpKWmp6ipqqusra6voKGys7S1tre4ubq7vL2+v7CxwsPExcbHyMnKy8zNzs/AwdLT1NXW19jZ2tvc3d7f0NHi4+Tl5ufo6err7OvloAADs=';
+      return desktopBase370px;
     }
   };
 
@@ -72,7 +72,7 @@ const LazyImage = ({ portfolioImg, img, isHidden }) => {
       />
 
       <img
-        className={`${'lazy'} ${portfolioImg} ${imgHidden ? isHidden : ''}`}
+        className={`${'lazy'} ${portfolioImg}`}
         src={baseImage()}
         data-src={imageDevice()}
         alt={alt}
